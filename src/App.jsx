ac230b585a13822856b865b1d533cbadc62d23b5
Aviscad <anthonyviscad@gmail.com>
2022-11-29 20:26:51 -0600

    Getting Pokemon IDs and PokemonCard Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,73 @@
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
+import {useState, useEffect} from "react";
+
+const getRandomInt = (min = 1, max = 152) => {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min) + min);
+};
+
+const getRandomArray = () => {
+	let pokemonIds = new Set();
+
+	for (let i = 0; i < 6; i++) {
+		let n = getRandomInt();
+
+		while (pokemonIds.has(n)) {
+			console.log("repeated: " + n);
+			n = getRandomInt();
+		}
+		pokemonIds.add(n);
+	}
+	return pokemonIds;
+};
+
+const shuffleArray = array => {
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[array[i], array[j]] = [array[j], array[i]];
+	}
+};
 
 function App() {
+	const [pokemonList, setPokemonList] = useState([]);
+	const [pokemonClicked, setPokemonClicked] = useState([]);
+
+	const handlePokemonClicked = pokemonId => {
+		if (pokemonClicked.indexOf(pokemonId) === -1) {
+			pokemonClicked.push(pokemonId);
+			setPokemonClicked([...pokemonClicked]);
+			shuffleArray(pokemonList);
+		} else {
+			setPokemonClicked([]);
+			console.log("Already Clicked!");
+		}
+		console.log(pokemonClicked);
+	};
+
+	useEffect(() => {
+		let ignore = false;
+		if (!ignore) {
+			setPokemonList([...getRandomArray()]);
+		}
+		return () => {
+			ignore = true;
+		};
+	}, []);
+
 	return (
 		<div className="App">
-			<h1 className="text-3xl font-bold underline m-1 shadow-lg">
-				Hi from Vite + Tailwind
-			</h1>
 			<div className="container flex gap-3 p-3">
-				<PokemonCard sprite="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/150.png" />
-				<PokemonCard sprite="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/149.png" />
-				<PokemonCard sprite="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/148.png" />
+				{pokemonList.map(pokemonNumber => (
+					<PokemonCard
+						number={pokemonNumber}
+						key={pokemonNumber}
+						handlePokemonClick={handlePokemonClicked}
+					/>
+				))}
+				<p>Clicks: {pokemonClicked.length}</p>
+				<p>{pokemonClicked.length === pokemonList.length ? "Won" : ""}</p>
 			</div>
 		</div>
 	);
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,20 @@
-function PokemonCard({sprite}) {
+function PokemonCard({number, handlePokemonClick}) {
 	return (
 		<div>
 			<img
-				className="shadow-lg w-36 h-36 cursor-pointer"
-				src={sprite}
+				className="shadow-md w-36 h-36 cursor-pointer"
+				src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${number}.png`}
+				alt=""
+				onClick={() => {
+					handlePokemonClick(number);
+					console.log(number);
+				}}
 			/>
 		</div>
 	);
 }
 
+/*
+ GET: https://pokeapi.co/api/v2/pokemon/{id or name}/
+*/
 export default PokemonCard;
